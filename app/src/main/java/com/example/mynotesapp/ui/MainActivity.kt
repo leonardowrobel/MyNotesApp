f7e26fc94c3ae9587d4a939077d1742a63ab7ca6
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2024-05-16 22:49:26 -0300

    cleaning

--- app/src/main/java/com/example/mynotesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/mynotesapp/ui/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.mynotesapp.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -15,37 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.mynotesapp.ui.home.HomeViewModel
 import com.example.mynotesapp.ui.theme.MyNotesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.ArrayList
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-//    private val mPermissions = arrayOf(Manifest.permission.)
-//    private val mRequestPermissions: MutableList<String> = ArrayList()
-
-//    private fun requestPermissions(): Boolean {
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            for (i in mPermissions.indices) {
-//                if (ContextCompat.checkSelfPermission(
-//                        this,
-//                        mPermissions[i]
-//                    ) != PackageManager.PERMISSION_GRANTED
-//                ) {
-//                    mRequestPermissions.add(mPermissions[i])
-//                }
-//            }
-//            if (mRequestPermissions.size > 0) {
-//                ActivityCompat.requestPermissions(this, mPermissions, PERMISSION_REQUEST_CODE)
-//                return false
-//            }
-//        }
-//        return true
-//    }
 
     private lateinit var mainViewModel: MainViewModel
 
@@ -53,8 +23,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
-        // requestPermissions()
 
         setContent {
             MyNotesAppTheme {
--- app/src/main/java/com/example/mynotesapp/ui/MainViewModel.kt
+++ app/src/main/java/com/example/mynotesapp/ui/MainViewModel.kt
@@ -4,8 +4,4 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
-    fun verifyIfHasSession(){
-
-    }
-
 }
