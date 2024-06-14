308ea5e9741db7322e3e4c84def315f719b138e2
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2024-06-14 16:22:20 -0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,10 +82,13 @@ dependencies {
 
     // ViewModel Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
 
     // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+//    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
 
     // Local Unit Tests
     implementation("androidx.test:core:1.5.0")
